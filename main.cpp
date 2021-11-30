e8974d6fd277d103bc3c0f64a39c99f978dc3093
Rafi Ahmed <rafi201822@gmail.com>
2021-12-01 03:23:10 +0600

    initial commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    int sum;
+
+    cout << "Please input the number:" << endl;
+    cin >> a;
+
+    cout << "Please input the number" << endl;
+    cin >> b;
+
+    sum = a + b;
+
+    cout << "Your result is " << sum << endl;
+
+    return 0;
+}
